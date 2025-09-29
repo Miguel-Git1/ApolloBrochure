19272fa9ce7b8850eac93d7e69f33d26617bdd6b
Miguel Machado <miguelmews123vv@gmail.com>
2025-09-29 15:53:43 +0100

    Remove unsused blueprint

--- brochure/brochure.py
+++ brochure/brochure.py
@@ -3,11 +3,9 @@ import json
 from flask import Flask
 
 from .authentication.routes import authentication_bp
-from .dashboard.routes import dashboard_bp
 
 brochure_app = Flask(__name__)
 brochure_app.config.from_file("../settings.json", load=json.load)
 
 # Blueprints
 brochure_app.register_blueprint(authentication_bp)
-brochure_app.register_blueprint(dashboard_bp)

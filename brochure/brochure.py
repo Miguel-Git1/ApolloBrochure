01d7dd6a384d4dea3576dc68efc58cb7ef51f963
Miguel Machado <miguelmews123vv@gmail.com>
2025-09-29 21:12:18 +0100

    Add global static files
    In order to mantain this clean, and reusable, we need to add global resources we might use across blueprints. For example, if we do use jquery in all blueprints, we might just spread acroos all blueprints and that eliminates the effort of importing one by one.

--- brochure/brochure.py
+++ brochure/brochure.py
@@ -1,10 +1,10 @@
 import json
 
-from flask import Flask
+from flask import Flask, render_template
 
 from .authentication.routes import authentication_bp
 
-brochure_app = Flask(__name__)
+brochure_app = Flask(__name__, static_folder="static", template_folder="template")
 brochure_app.config.from_file("../settings.json", load=json.load)
 
 # Blueprints
